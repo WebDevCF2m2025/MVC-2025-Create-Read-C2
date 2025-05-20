e0df0c6335ad79f76753fa9fe7cd91b8f6733510
reda. <redadahmani34@gmail.com>
2025-05-20 12:14:32 +0200

    reda pas fini..

--- public/index.php
+++ public/index.php
@@ -11,6 +11,21 @@ session_start();
 require_once "../config-dev.php";
 
 // notre connexion PDO
+try {
+    $db = new PDO(DB_DSN, DB_LOGIN, DB_PWD);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
+}
+
+if(isset($_SESSION['user_login'])) {
+  require_once "../controller/PrivateController.php";
+}else {
+  require_once "../controller/PublicController.php";
+}
 
 // ici nos contrôleurs
 
@@ -25,3 +40,4 @@ var_dump($_POST);
 
 
 // bonne pratique
+$db = null;
