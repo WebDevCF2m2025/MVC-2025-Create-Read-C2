2ae1d2d22bbead18a7234a4aa87dc7abb9a0e53b
NikGorban <masarash12345@gmail.com>
2025-05-20 11:19:14 +0200

    inport connection, boostrap

--- public/index.php
+++ public/index.php
@@ -5,17 +5,43 @@
  * Contrôleur frontal
  */
 
+
+// création de la session (ou continuation)
+// crée un cookie nommé PHPSESSID côté utilisateur
+// crée un fichier texte lié côté serveur
 session_start();
 
 // dépendances
 require_once "../config-dev.php";
 
-// notre connexion PDO
+try{
+    // instanciation de PDO
+    $db = new PDO(DB_DSN,DB_LOGIN,DB_PWD);
+    // par défaut, on obtient des tableaux associatifs
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+    // on active l'affichage des erreurs
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-// ici nos contrôleurs
+}catch(Exception $e){
+    die($e->getMessage());
+}
+
+// si nous sommes connectés
+if(isset($_SESSION['userlogin']))
+{
+    // nous prenons le contrôleur privé
+    require_once "../controller/PrivateController.php";
+
+// non connectés
+}else {
+    // chargement du routeur public
+    require_once "../controller/PublicController.php";
+}
+
 
 echo '<h4>session_id() ou SID</h4>';
 var_dump(session_id());
+var_dump($db);
 echo '<h4>$_GET</h4>';
 var_dump($_GET);
 echo '<h4>$_SESSION</h4>';
@@ -25,3 +51,4 @@ var_dump($_POST);
 
 
 // bonne pratique
+$db=null;
